2f93deae19a4692dbd9c47324d057536b9328a10
unknown <anthony_rosati@yahoo.com>
2023-11-16 18:51:51 -0600

    Added inquirer module

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
+console.log`${process.argv}`
+
+// Adds fs library
+const fs = require('fs');
+// Adds inquirer library
+const inquirer = require('inquirer')
+
+console.log(fs)
+console.log(inquirer)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "question one",
+    "question two"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    questions.forEach(question => console.log(question))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
